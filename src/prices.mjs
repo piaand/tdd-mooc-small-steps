e5ca11d94f740def44b6cf11c18d0baf07874dab
Pia Andersin <pia.andersin@futurice.com>
2022-01-05 17:12:49 +0200

    remove convertor from calculate reduction

--- src/prices.mjs
+++ src/prices.mjs
@@ -50,8 +50,7 @@ function createApp(database) {
 
   function calculateReduction(date) {
     let reduction = 0;
-    const tempDate = dateToTemporalPlain(date)
-    if (tempDate && isMonday(tempDate) && !isHoliday(tempDate)) {
+    if (date && isMonday(date) && !isHoliday(date)) {
       reduction = 35;
     }
     return reduction;
